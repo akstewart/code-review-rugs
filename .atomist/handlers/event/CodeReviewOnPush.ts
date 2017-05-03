13a3f271ceb595837de4eaa1af1e0028e6ecc266
Alan Stewart <alankstewart@gmail.com>
2017-05-03 14:58:57 +1000

    adds a TypeScript Rug event handler to a Rug project

---
+++ .atomist/handlers/event/CodeReviewOnPush.ts
@@ -0,0 +1,20 @@
+import { EventHandler, Tags } from "@atomist/rug/operations/Decorators";
+import { ChannelAddress, DirectedMessage, EventPlan, HandleEvent } from "@atomist/rug/operations/Handlers";
+import { Match } from "@atomist/rug/tree/PathExpression";
+
+import { Tag } from "@atomist/cortex/Tag";
+
+/**
+ * A Run code analysis.
+ */
+@EventHandler("CodeReviewOnPush", "Run code analysis", "/Tag()")
+@Tags("documentation")
+export class CodeReviewOnPush implements HandleEvent<Tag, Tag> {
+    public handle(event: Match<Tag, Tag>): EventPlan {
+        const root: Tag = event.root;
+        const message = new DirectedMessage(`${root.nodeName()} event received`, new ChannelAddress("#general"));
+        return EventPlan.ofMessage(message);
+    }
+}
+
+export const codeReviewOnPush = new CodeReviewOnPush();
